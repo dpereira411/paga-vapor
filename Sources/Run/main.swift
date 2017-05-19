428f179d405098548cfcc3bd7b9a05cd7375cf02
Ricardo Torrão <rtorrao@imaginarycloud.com>
2017-05-19 18:25:49 +0100

    add Cors rules

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -5,8 +5,10 @@ let config = try Config()
 try config.setup()
 
 try config.addProvider(PostgreSQLProvider.Provider.self)
+config.addConfigurable(middleware: CORSMiddleware(), name: "cors")
 
 let drop = try Droplet(config)
 try drop.setup()
+
 
 try drop.run()
